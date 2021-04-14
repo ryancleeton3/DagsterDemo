c419b6ff3eb76a1db0c0d78fa2846b393fa71f3b
Ryan Cleeton <wrc089@gmail.com>
2021-04-14 12:04:29 -0400

    a two solid serial pipeline example

--- repos.py
+++ repos.py
@@ -4,12 +4,18 @@ import os
 from dagster import solid, pipeline
 
 @solid
-def hello_cereal(context):
+def load_cereals(context):
     dataset_path = os.path.join(os.path.dirname(__file__), 'cereal.csv')
     with open(dataset_path, 'r') as f:
         cereals = [row for row in csv.DictReader(f)]
     context.log.info(f'Found {len(cereals)} cereals.')
+    return cereals
+
+@solid
+def sort_by_calories(context, cereals):
+    sorted_cereals = list(sorted(cereals, key=lambda cereal: cereal['calories']))
+    context.log.info(f'Most Caloric Cereal: {sorted_cereals[-1]["name"]}')
 
 @pipeline
-def hello_cereal_pipeline():
-    hello_cereal()
+def serial_pipeline():
+    sort_by_calories(load_cereals())
